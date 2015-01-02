af9de0b55fa7aa7fd1bc11635cb89ca87ae97746
ruhadaemini <ruhada@hotmail.com>
2015-01-02 12:03:34 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en"> 
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see www.w3.org">
+<title>Bus Travel</title>
+<link rel="stylesheet" type="text/css" href="cssfile.css">
+    <script src="sliderengine/jquery.js"></script>
+    <script src="sliderengine/amazingslider.js"></script>
+    <script src="sliderengine/initslider-1.js"></script>   
+</head>
+<body>
+    <div id="page">
+        <div id="header">
+            <div id="logo">
+                <img src="images/logoo.png" alt="logo">
+            </div>
+            
+            <div id="menu">
+                <ul>
+                    <li><a href='ballina.php'><img src='images/home.png'><span>Ballina</span></a></li>
+                    <li><a href='readqytetet.php'><img src='images/qytete.png'><span>Qytetet</span></a></li>
+                    <li><a href='readagjensionet.php'><img src='images/bus.png'><span>Agjensionet</span></a></li>
+                    <li><a href='readlinja.php'><img src='images/linja.png'><span>Linjat</span></a></li>
+                </ul>
+            </div>   
+        </div>
